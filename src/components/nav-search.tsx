4ea85fbe0b19e8d3fb6bdb52a0a338df1d6dfef4
Hicham1970 <devhicham2@gmail.com>
2025-05-15 14:06:53 +0100

    Emplacement du CC

--- src/components/nav-search.tsx
+++ src/components/nav-search.tsx
@@ -1,6 +1,8 @@
+"use client";
+import React, { useState } from 'react';
 
 export const NavSearch = () => {
-    
+    const [searchTerm, setSearchTerm] = useState('');
 
     console.log('NavSearch component rendered');
     return (
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,9 @@
-"use client";
-
 import { NavLinks } from "./nav-links";
 import { NavSearch } from "./nav-search";
-import { useState } from "react";
+
 
 export const Navbar = () => {
-    const [search, setSearch] = useState("")
-    console.log('Navbar component rendered');
+        console.log('Navbar component rendered');
     return (
         <div>
             <NavLinks />
